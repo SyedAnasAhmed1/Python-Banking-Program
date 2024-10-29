19d4de9a2af1a6e154220fd57d28194faa63eb10
Syed Anas Ahmed <syedanasahmed7@gmail.com>
2024-10-29 21:21:36 +0500

    Create main.py
    Basic Python Banking Program

---
+++ main.py
@@ -0,0 +1,69 @@
+#Python Banking Program
+
+def show_balance(balance):
+    print("*********************")
+    print(f"Your Balance is ${balance:.2f}")
+    print("*********************")
+
+def deposit():
+    print("*********************")
+    amount = float(input("Enter an amount to deposited:"))
+    print("*********************")
+    if amount < 0:
+        print("*********************")
+        print("That's not a valid amount")
+        print("*********************")
+        return 0
+    else:
+        return amount
+    
+def withdraw(balance):
+    print("*********************")
+    amount = float(input("Enter amount to be withdrawn: "))
+    print("*********************")
+    
+    if amount > balance:
+        print("*********************")
+        print("Insufficient funds")
+        print("*********************")
+        return 0
+    elif amount < 0:
+        print("Amount must be greater than 0")
+        return 0
+    else:
+        return amount
+
+def main():
+    balance = 0
+    is_running = True
+    
+    while is_running:
+        print("*********************")
+        print("   Banking Program   ")
+        print("*********************")
+        print("1.Show Balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+        print("*********************")
+        choice = input("Enter Your Choice (1-4):")
+    
+        if choice == "1":
+            show_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdraw(balance)
+        elif choice == "4":
+            is_running = False
+        else:
+            print("*********************")
+            print("That is Not a valid Choice")
+            print("*********************")
+    
+    print("*********************")    
+    print("Thank You having a nice day!")
+    print("*********************")
+    
+if __name__ == '__main__':
+    main()
